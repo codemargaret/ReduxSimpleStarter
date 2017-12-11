084d4703437ce9f51ab8696b0fc4d1970be5ae73
Margaret Berry <codeberry1@gmail.com>
2017-12-10 19:23:27 -0800

    refactor using es6 syntax

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
