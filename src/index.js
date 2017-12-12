6d7ffe7e331efa43dab60a1c0f282bf413927353
Margaret Berry <codeberry1@gmail.com>
2017-12-11 21:59:06 -0800

    show videos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBG2rI95W-sVbKVX6kaHRnkT7zMWCq1Cjc'
 
 //Create a new component. This component should produce some HTML.
-const App = () => {
-  return <div>Hi!</div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // same as: this.setState({videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it on the page(in the DOM).
